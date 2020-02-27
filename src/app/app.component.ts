ecf4fb99e54e318a308b592937c125aee858f09f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-27 08:46:24 +0000

    Deploy feb-git-2021/my-repo to github.com/feb-git-2021/my-repo.git:gh-pages

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Continuous Integration With TRAVIS CI';
+   test ='Wrong usgae of var keyword'
+}
